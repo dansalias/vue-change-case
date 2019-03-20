14df76ae9395039da46573740733a6247089cd8c
dansalias <danyoungnz@gmail.com>
2019-03-21 10:28:54 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
+import methodExtract from 'method-extract';
 import changeCase from 'change-case';
 
-// Extract all methods matching *Case* from changeCase
-const extractMethods = () => {
-  const methodNames = [];
-  for (const methodName in changeCase) {
-    if (
-      typeof changeCase[methodName] === "function" &&
-      methodName.includes('Case')
-    ) {
-      methodNames.push(methodName);
-    }
-  }
-  return methodNames;
-};
-
-// Add methods as Vue filters
+// Add methods containing 'Case' as Vue filters
 const plugin = {
   install(Vue) {
-    extractMethods().forEach((methodName) => {
+    methodExtract.extractFrom(changeCase, /Case/).forEach((methodName) => {
       Vue.filter(methodName, value => changeCase[methodName](value));
     })
   }
